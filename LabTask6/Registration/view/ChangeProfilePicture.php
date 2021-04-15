7bea6ded938edfbac075cb5c11f805fae49faaa5
Fabiha99 <77955807+Fabiha99@users.noreply.github.com>
2021-04-15 17:30:40 +0600

    Add files via upload

---
+++ LabTask6/Registration/view/ChangeProfilePicture.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<div class="menu">
+<?php include 'menu.php';?>
+<form action="upload2.php" method="post" enctype="multipart/form-data">
+ <h1> Profile Picture</h1>
+ <br><br>
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload Image" name="submit">
+</form>
+<?php include 'account.php';?>  
+ <?php include 'footer.php';?> 
+
+</body>
+</html>
