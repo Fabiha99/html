38785ae389b55223a05f1d839be746fef1ccf30d
Fabiha99 <77955807+Fabiha99@users.noreply.github.com>
2021-04-15 16:47:40 +0600

    Add files via upload

---
+++ LabTask6/view/account.php
@@ -0,0 +1,10 @@
+<?php
+echo '<a href="welcome.php"><ul><li>Dasboard</li></ul></a> ';
+echo '<a href="viewprofile.php"><ul><li>View Profile</li></ul></a> ';
+echo '<a href="editprofile.php"><ul><li>Edit Profile</li></ul></a> ';
+echo '<a href="ChangeProfilePicture.php"><ul><li>Change Profile Picture</li></ul></a> ';
+echo '<a href="Changepass.php"><ul><li>Change Password</li></ul></a> ';
+echo '<a href="logout.php"><ul><li>Logout</li></ul></a> ';
+
+
+?>
